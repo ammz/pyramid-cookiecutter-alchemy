57b198aca63b06b0a9c398b07ef83764a8e43d81
Antonio Moreno <ammz@deversorius.net>
2018-02-15 22:58:21 +0100

    fix typo error

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -5,7 +5,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_pt')
+    config.include('pyramid_chamaleon')
     config.include('.models')
     config.include('.routes')
     config.scan()
